2e6185cf6062f7dfe6faf0bc1fbd104c72f1390e
Luca Hummel <luca.hummel12@gmail.com>
2021-11-03 15:33:26 -0300

    Created example charts

--- AreaExample/AreaExample/ContentView.swift
+++ AreaExample/AreaExample/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
                             .font(.title)
                             .bold()
                             .padding()
-                        AreaGraph()
+                        AreaGraph(datas: [[29, 72, 98, 69, 140, 136, 808, 2277, 1663, 95, 0, 0], [16, 103, 77, 228, 280, 106, 598, 1195, 725, 55, 0, 0]], titulo: "Monitoramento dos Focos Ativos por Estado", legendas: ["São Paulo", "Paraná"], nomeseixoX: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"])
                     }
                 } .navigationTitle("Gráfico de Área")
             }
