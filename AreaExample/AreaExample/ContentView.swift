7af749228ed20fae3808496998f9f89d42ef9d4c
Luca Hummel <luca.hummel12@gmail.com>
2021-11-03 17:39:49 -0300

    Added chart

--- AreaExample/AreaExample/ContentView.swift
+++ AreaExample/AreaExample/ContentView.swift
@@ -27,7 +27,12 @@ struct ContentView: View {
                             .font(.title)
                             .bold()
                             .padding()
-                        AreaGraph(datas: [[29, 72, 98, 69, 140, 136, 808, 2277, 1663, 95, 0, 0], [16, 103, 77, 228, 280, 106, 598, 1195, 725, 55, 0, 0]], titulo: "Monitoramento dos Focos Ativos por Estado", legendas: ["São Paulo", "Paraná"], nomeseixoX: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"])
+                        AreaGraph(datas: [[29, 72, 98, 69, 140, 136, 808, 2277, 1663, 95, 0, 0], [16, 103, 77, 228, 280, 106, 598, 1195, 725, 55, 0, 0]], titulo: "Monitoramento dos Focos Ativos por Estado", legendas: ["São Paulo", "Paraná"], nomeseixoX: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"], alturaGrafico: 150)
+                        Text("Exemplo 3")
+                            .font(.title)
+                            .bold()
+                            .padding()
+                        AreaGraph(datas: [[10,15,60,5], [60,70,23,10]], titulo: "Chart", legendas: ["Roxo", "Preto"], nomeseixoX: ["0","1","2","4"], colors: [Color.purple, Color.black], marginDireita: 40, marginEsquerda: 40, alturaGrafico: 300, quantidadeY: 10)
                     }
                 } .navigationTitle("Gráfico de Área")
             }
