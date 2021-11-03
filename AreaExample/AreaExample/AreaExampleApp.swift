0a463e8e749d225bb706ee39a82bcbe7161f7c5f
Luca Hummel <luca.hummel12@gmail.com>
2021-11-03 15:10:39 -0300

    Create project

---
+++ AreaExample/AreaExample/AreaExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaExampleApp.swift
+//  AreaExample
+//
+//  Created by Luca Hummel on 03/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AreaExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AreaExample/AreaExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AreaExample
+//
+//  Created by Luca Hummel on 03/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
